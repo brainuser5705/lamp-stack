9badcdd3d4743c2ef71179934fb932fbbbca48da
brainuser5705 <codeuser5705@outlook.com>
2021-07-19 18:50:21 -0400

    missing parsedown autoload 4

--- status-updates/index.php
+++ status-updates/index.php
@@ -12,7 +12,7 @@
     $statusContent = "";
     if (!empty($statuses)){
 
-        require '../../vendor/autoload.php';
+        require __DIR__ . '/vendor/autoload.php';
         $Parsedown = new Parsedown();
 
         foreach($statuses as $status){
