345fcfb1bcd5347cc4f5edf70245e4b6e3045b7c
brainuser5705 <codeuser5705@outlook.com>
2021-07-20 23:05:44 -0400

    converting utc times into est times 8

--- templates/status-updates.php
+++ templates/status-updates.php
@@ -7,6 +7,7 @@
     </div>
     <div class="status-update-datetime">
         - <?php 
+                include $_SERVER['DOCUMENT_ROOT'] . '/abstraction/convert_datetime.php';
                 echo convert_datetime($status->getDatetime()); 
             ?>
     </div>
