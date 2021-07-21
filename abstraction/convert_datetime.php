4b12e2ed6933f5032fca3ca9d239b63a066379f7
brainuser5705 <codeuser5705@outlook.com>
2021-07-20 22:02:11 -0400

    converting utc times into est times 2

--- abstraction/convert_datetime.php
+++ abstraction/convert_datetime.php
@@ -2,8 +2,15 @@
 
     function convert_datetime($datetime){
 
-        $new_datetime = date("m/d/y (h:i a)", strtotime($datetime));
+        //formatting datetime 
+        $datetime = date("m/d/y (h:i a)", strtotime($datetime));
+
+        // making into datetime object
+        $new_datetime = new Datetime($datetime);
+    
+        //changing timezone
         $new_datetime->setTimezone('America/New_York');
+        
         return $new_datetime;
 
     }
