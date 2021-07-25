4f2beb8af4909c0d1e18545fdda718b98c4bd9fa
brainuser5705 <codeuser5705@outlook.com>
2021-07-25 11:23:05 -0400

    missing status updates template

---
+++ templates/status-updates.php
@@ -0,0 +1,14 @@
+<div class="status-update">
+    <div class="status-update-text">
+        <?php 
+            $text = $status->getText(); 
+            echo $Parsedown->text($text);
+        ?>
+    </div>
+    <div class="status-update-datetime">
+        - <?php 
+                include_once($_SERVER['DOCUMENT_ROOT'] . '/abstraction/convert_datetime.php');
+                echo convert_datetime($status->getDatetime()); 
+            ?>
+    </div>
+</div>
