12b6eafc2c8fb541029b017bb4b66c96f24d4e42
brainuser5705 <codeuser5705@outlook.com>
2021-08-03 00:55:15 -0400

    fixes and stuff

--- templates/admin/admin-top.php
+++ templates/admin/admin-top.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title><a href="/">brainuser5705 | Admin</a></title>
+        <title>brainuser5705 | Admin</title>
         <link rel="stylesheet" href="/static/css/layout.css">
         <link rel="stylesheet" href="/static/css/styles.css">
         <link rel="stylesheet" href="/static/css/admin.css">
@@ -11,17 +11,16 @@
 
         <div class="visible">
             <header>
-                <h1 id="website-name">brainuser5705</h1>
+                <h1 id="website-name"><a href="/">brainuser5705</a></h1>
                 <div>Admin Page</div>
             </header>
 
             <div class="visible-body">
-                
+
                 <nav>
                     <hr>
                     <a href="/admin/">Forms</a>
                     <a href="/admin/modify.php">Modify</a>
-                    Statuses
                 </nav>
 
                 <a href="/admin/logout.php">Logout</a>
